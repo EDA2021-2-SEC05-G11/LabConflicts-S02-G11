f6ad373337887c2b80dc2b02043b8e26d9f2848e
Lina Ojeda <l.ojedaa@uniandes.edu.co>
2021-08-24 16:19:25 -0500

    cambios en el estudiante 2

--- App/view.py
+++ App/view.py
@@ -83,14 +83,18 @@ while True:
 
 
         # TODO: Modificación de Est-2 en el Lab 2
+        print('Último libro cargado: ' + str(lt.lastElement(books)))
 
     elif int(inputs[0]) == 2:
         print("Cargando información de tags....")
         tags = loadTags()
         print('Total de tags cargados: ' + str(lt.size(tags)))
-    elif True:
-        # TODO: Modificación de Est-2 en el Lab 2
-        pass
+
+        # TODO: Modificación del Est-2 en el Lab 2
+    elif int(inputs[0]) == 3:
+        print("Cargando información de Book-Tags...")
+        booktags = loadBookTags()
+        print('Total de Book-Tags cargados: ' + str(lt.size(booktags)))
 
     else:
         sys.exit(0)
